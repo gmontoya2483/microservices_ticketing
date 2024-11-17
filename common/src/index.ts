b02fad1bf409a2ae10e766c3269b3269e8392b11
Gabriel Montoya <gabi.montoya@gmail.com>
2024-11-17 09:46:20 -0300

    additional config

---
+++ common/src/index.ts
@@ -0,0 +1,16 @@
+interface Color {
+    red: number;
+    green: number;
+    blue: number;
+}
+
+const color: Color = {
+    red: 10,
+    blue: 10,
+    green: 5,
+};
+
+
+console.log(color);
+
+export default color;
